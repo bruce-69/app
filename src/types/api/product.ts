6a54c615b63111418879cbcf77a3de9dba9cffd6
jorge <eduardcrow555@gmail.com>
2023-03-26 14:02:45 -0300

    final

---
+++ src/types/api/product.ts
@@ -0,0 +1,22 @@
+export interface Product {
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+  }
+  
+  export interface ProductUpdate {
+    product: Product;
+    id: string;
+  }
+  
+  export interface ProductResponse {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+    updatedAt?: string;
+    createdAt?: string;
+  }
+  
---
+++ src/types/orderType.ts
@@ -0,0 +1,7 @@
+export enum OrderType {
+    COMER_NO_LOCAL = "Comer no local",
+    PARA_VIAGEM = "Para viagem",
+    DELIVERY = "Delivery",
+  }
+  
+  
